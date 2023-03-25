a30b21eecb7060c4ea2ab1cee5193340b24731c6
RQD-china <1245413737@qq.com>
2023-03-25 17:41:55 +0800

    feat: plugin

---
+++ build/plugin/index.js
@@ -0,0 +1,37 @@
+import vue from '@vitejs/plugin-vue'
+
+/**
+ * * 扩展setup插件，支持在script标签中使用name属性
+ * usage: <script setup name="MyComp"></script>
+ */
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+
+// rollup打包分析插件
+import { visualizer } from 'rollup-plugin-visualizer'
+
+// index.html压缩注入插件
+import { configHtmlPlugin } from './html.js'
+
+// css库
+import { unocss } from './unocss.js'
+
+export function createVitePlugins(viteEnv, isBuild) {
+  const plugins = [
+    vue(),
+    VueSetupExtend(),
+    configHtmlPlugin(viteEnv, isBuild),
+    unocss(),
+  ]
+  
+  if (isBuild) {
+    plugins.push(
+      visualizer({
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      })
+    )
+  }
+
+  return plugins
+}
