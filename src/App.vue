40968e5691c1bb7a705d7129a84b32fb6caf7992
RQD-china <1245413737@qq.com>
2023-03-25 17:12:54 +0800

    feat: enviroment

--- src/App.vue
+++ src/App.vue
@@ -28,3 +28,9 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<style lang="scss">
+#app {
+  background-color: $primaryColor;
+}
+</style>
